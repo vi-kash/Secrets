f8c46118afb2b90932777278362b3e8e89777643
Vikash Kumar <21je1044@iitism.ac.in>
2023-10-09 10:41:51 +0530

    Implement Hashing Password

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import ejs from "ejs";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import encrypt from "mongoose-encryption";
+import md5 from "md5";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -23,7 +24,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -42,7 +43,7 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
     const newUser = new User({
         email: req.body["username"],
-        password: req.body["password"]
+        password: md5(req.body["password"])
     });
     newUser.save().then(() => {
         res.render("secrets");
@@ -53,7 +54,7 @@ app.post("/register", (req, res) => {
 
 app.post("/login", (req, res) => {
     const username = req.body["username"];
-    const password = req.body["password"];
+    const password = md5(req.body["password"]);
     User.findOne({email: username}).then((foundUser) => {
         if(foundUser && foundUser.password === password){
             res.render("secrets");
